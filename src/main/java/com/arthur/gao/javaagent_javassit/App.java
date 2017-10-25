ec2301e463fbb458ddc400bc3d18910facc8f5e9
pan.gao <arthur.gao@biz-united.com.cn>
2017-10-25 10:06:09 +0800

    init

---
+++ src/main/java/com/arthur/gao/javaagent_javassit/App.java
@@ -0,0 +1,13 @@
+package com.arthur.gao.javaagent_javassit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
